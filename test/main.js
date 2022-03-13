0030277787d1f20a6fab29441e8a16bddd13c8fe
hamodi165 <hamodijaafary@outlook.com>
2022-03-13 02:51:22 +0100

    Startat ett nytt projekt, gaming polls

---
+++ test/main.js
@@ -0,0 +1,21 @@
+//Comment
+function commentFunction() {
+    var x = document.getElementById("comment-area");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function replyFunction() {
+    var x = document.getElementById("reply-area");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+    
+
+
