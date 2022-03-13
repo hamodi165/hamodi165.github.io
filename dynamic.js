7a704166c43181397654f8b21a9c77e56932bad4
hamodi165 <hamodijaafary@outlook.com>
2022-03-13 03:52:09 +0100

    testt

---
+++ dynamic.js
@@ -0,0 +1,29 @@
+document.getElementById('loginButton').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
+
+document.getElementById('shoppingButton').addEventListener("click", function() {
+	document.querySelector('.shopping-cart').style.display = "flex";
+});
+
+document.querySelector('.close-shopping').addEventListener("click", function() {
+	document.querySelector('.shopping-cart').style.display = "none";
+});
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("#sneakers").hide();
+  });
+  $("#show").click(function(){
+    $("#sneakers").show();
+  });
+});
+
+
+
+
+
