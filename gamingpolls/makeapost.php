7384c8c91781e0d7452ef92ec9d4c6640285ce8c
hamodi165 <43251737+hamodi165@users.noreply.github.com>
2022-05-14 23:38:09 +0200

    summernote fixat

--- gamingpolls/makeapost.php
+++ gamingpolls/makeapost.php
@@ -7,13 +7,32 @@
 
 <div class="thread">
   <form action="/action_page.php">
-    <input type="text" id="posttitle" name="posttitle" placeholder="Your last name..">  <br>
-    <textarea id="subject" name="subject" placeholder="Text (Optional)"></textarea>
-    <br>
+    <div class="thetitle">
+    <input type="text" class="posttitle" name="posttitle" placeholder="Title"> <br>
+  </div>
+    <textarea id="summernote" name="subject"></textarea>
     <input type="submit" value="Submit">
   </form>
 </div>
 
+
+
+<script> $(document).ready(function() {
+  $('#summernote').summernote({
+        placeholder: 'Text (Optional)',
+        tabsize: 2,
+        height: 160,
+        toolbar: [
+          ['style', ['style']],
+          ['font', ['bold', 'underline', 'clear']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['table', ['table']],
+          ['insert', ['link', 'picture', 'video']],
+          ['view', ['fullscreen', 'codeview', 'help']]
+        ]
+      });
+}); </script>
 
 </body>
 
